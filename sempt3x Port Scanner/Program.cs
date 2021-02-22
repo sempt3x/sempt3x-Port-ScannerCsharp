a06a9dc09847e7cafd4b405b3cd721c5d35aac38
sempt3x <32197873+sempt3x@users.noreply.github.com>
2021-02-22 18:20:26 +0100

    Update Program.cs
    Adding SendTimeout for faster Check

--- sempt3x Port Scanner/Program.cs
+++ sempt3x Port Scanner/Program.cs
@@ -5,7 +5,7 @@
 using System;
 using System.Net.Sockets;
 
-namespace ConsoleApp3
+namespace sempt3x_Port_Scanner
 {
     internal class Program
     {
@@ -15,8 +15,8 @@ namespace ConsoleApp3
             string ipeingabe = "";
             int port1 = 0, port2 = 0;
 
-            Console.WriteLine("SEMPT3X Port Scanner v1.0.0 \n\n"); // Programm Console Beginning Text
-            Console.Title = "SEMPT3X Port Scanner v1.0.0";
+            Console.WriteLine("SEMPT3X Port Scanner v1.0.1 \n\n"); // Programm Console Beginning Text
+            Console.Title = "SEMPT3X Port Scanner v1.0.1";
 
             Console.Write("Enter the IP address: "); // IP Adress Input
             ipeingabe = Convert.ToString(Console.ReadLine());
@@ -30,18 +30,21 @@ namespace ConsoleApp3
             Console.WriteLine("\n");
             do // Begin Do While Loop
             {
+                TcpClient client = new TcpClient();
                 port1++;
                 try
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    TcpClient client = new TcpClient(ipeingabe, port1);
+                    client.Connect(ipeingabe,port1);
+                    client.SendTimeout = 1;
                     Console.WriteLine("[TCP-Port: " + port1 + " ] Port is open!");
                 }
                 catch (SocketException)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("[TCP-Port: " + port1 + " ] Port is not open!");
-                }
+                    client.SendTimeout = 1;
+                } 
             } while (port1 < port2); // End Do While Loop
 
             Console.ForegroundColor = ConsoleColor.White;
