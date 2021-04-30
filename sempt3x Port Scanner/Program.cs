05a859e9facfceccbd0ce83415f6f0c8bf472076
sempt3x <32197873+sempt3x@users.noreply.github.com>
2021-04-30 14:52:04 +0200

    Update Program.cs
    Add Global Variables and Add Catch Exception

--- sempt3x Port Scanner/Program.cs
+++ sempt3x Port Scanner/Program.cs
@@ -7,46 +7,71 @@ using System.Net.Sockets;
 
 namespace sempt3x_Port_Scanner
 {
+    class CheckStatus {
+        public string ipeingabe = "";
+        public int port_1 = 0, port_2 = 0;
+        public bool checkinput1 = true, checkinput2 = true;
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("SEMPT3X Port Scanner v1.0.2 \n\n"); // Programm Console Beginning Text
-            Console.Title = "SEMPT3X Port Scanner v1.0.2";
+            CheckStatus chs = new CheckStatus();
+
+            Console.WriteLine("SEMPT3X Port Scanner v1.1.0 \n\n"); // Programm Console Beginning Text
+            Console.Title = "SEMPT3X Port Scanner v1.1.0";
             do
             {
                 //* Define Variables *//
-                string ipeingabe = "";
-                int port_1 = 0, port_2 = 0;
 
                 Console.Write("Enter the IP address: "); // IP Adress Input
-                ipeingabe = Convert.ToString(Console.ReadLine());
+                chs.ipeingabe = Convert.ToString(Console.ReadLine());
 
+                do {
                 Console.Write("Enter the first port: "); // Port1 Input
-                port_1 = Convert.ToInt32(Console.ReadLine());
-
+                try {
+                    chs.port_1 = Convert.ToInt32(Console.ReadLine());
+                        if(String.IsNullOrEmpty(chs.port_1.ToString())) {
+                            chs.checkinput1 = true;
+                        } else {
+                            chs.checkinput1 = false;
+                        }
+                }
+                catch(System.FormatException) {
+                    Console.WriteLine("Failed!");
+                }
                 Console.Write("Enter the second port: "); // Port2 Input
-                port_2 = Convert.ToInt32(Console.ReadLine());
+                try {
+                    chs.port_2 = Convert.ToInt32(Console.ReadLine());
+                        if(String.IsNullOrEmpty(chs.port_2.ToString())) {
+                            chs.checkinput2 = true;
+                        } else {
+                            chs.checkinput2 = false;
+                        }
+                } catch {
+                    Console.WriteLine("Failed!");
+                }
+                }while(chs.checkinput1 == true || chs.checkinput2 == true);
 
                 Console.WriteLine("\n");
                 do // Begin Do While Loop
                 {
                     TcpClient client = new TcpClient();
-                    port_1++;
+                    chs.port_1++;
                     try
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        client.Connect(ipeingabe, port_1);
+                        client.Connect(chs.ipeingabe, chs.port_1);
                         client.SendTimeout = 1;
-                        Console.WriteLine("[TCP-Port: " + port_1 + " ] Port is open!");
+                        Console.WriteLine("[TCP-Port: " + chs.port_1 + " ] Port is open!");
                     }
                     catch (SocketException)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("[TCP-Port: " + port_1 + " ] Port is not open!");
+                        Console.WriteLine("[TCP-Port: " + chs.port_1 + " ] Port is not open!");
                         client.SendTimeout = 1;
                     }
-                } while (port_1 < port_2); // End Do While Loop
+                } while (chs.port_1 < chs.port_2); // End Do While Loop
 
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("\n\n");
