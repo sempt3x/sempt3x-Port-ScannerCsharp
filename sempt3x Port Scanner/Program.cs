4ab535cb2ff536a596fb1dbd7fd027d0147df419
Denis Zielonka <sempt3xx@gmail.com>
2021-07-26 13:01:58 +0200

    Einige Änderungen vorgenommen

--- sempt3x Port Scanner/Program.cs
+++ sempt3x Port Scanner/Program.cs
@@ -11,77 +11,59 @@ namespace sempt3x_Port_Scanner
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White; // Set Start Color to White
-            //* Create Object *//
-            var chs = new StatusHandler();
 
             //* Application Information *//
+
             Console.WriteLine(" SEMPT3X Port Scanner v1.2.0 \n"); // Program Console Beginning Text
             Console.Title = "SEMPT3X Port Scanner v1.2.0"; // Program Title Text
 
             //* Begin Frist Loop **//
+
             do
             {
                 Console.Write("\n Enter the IP address: "); // IP Adress Input
+
                 //** Begin Second Loop **/
+
                 do {
-                    if(string.IsNullOrEmpty(chs.Ipeingabe)) {
-                        chs.Checkinput1 = true;
-                        chs.Ipeingabe = Convert.ToString(Console.ReadLine());
+                    if(string.IsNullOrEmpty(StatusHandler.Ipeingabe)) {
+                        StatusHandler.Checkinput1 = true;
+                        StatusHandler.Ipeingabe = Convert.ToString(Console.ReadLine());
                     } else {
-                        chs.Checkinput1 = false;
+                        StatusHandler.Checkinput1 = false;
                     }
-                }while(chs.Checkinput1 == true);
+                }while(StatusHandler.Checkinput1 == true);
+
                 //** End Second Loop **//
+
                 //** Begin Third Loop **//
+
                 do {
-                    Console.Write(" Enter the first port: "); // Port 1 Input
-                    try
-                    {
-                        chs.Port1 = Convert.ToInt32(Console.ReadLine());
-                        chs.Checkinput2 = string.IsNullOrEmpty(chs.Port1.ToString());
-                    }
-                    catch(FormatException) {
-                        Console.WriteLine(" Failed!");
-                    }
-                    Console.Write(" Enter the second port: "); // Port 2 Input
-                    try
-                    {
-                        chs.Port2 = Convert.ToInt32(Console.ReadLine());
-                        chs.Checkinput3 = string.IsNullOrEmpty(chs.Port2.ToString());
-                    } catch {
-                        Console.WriteLine(" Failed!");
-                    }
-                }while(chs.Checkinput2|| chs.Checkinput3);
+                    StatusHandler.InputValues();
+                }while(StatusHandler.Checkinput2|| StatusHandler.Checkinput3);
+
                 //** End Third Loop **//
+
                 Console.WriteLine("\n");
+
                 //** Begin Fourth Loop **//
+
                 do
                 {
-                    var client = new TcpClient();
-                    chs.Port1++;
-                    
-                        var result = client.BeginConnect(chs.Ipeingabe, chs.Port1, null, null);
-                        var sucess = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
-                        if (!sucess)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red; // If Port closed -> Red
-                            Console.WriteLine(" [TCP-Port: " + chs.Port1 + " ] Port is not open!");
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green; // If Port open -> Green
-                            Console.WriteLine(" [TCP-Port: " + chs.Port1 + " ] Port is open!");
-                        }
-                } while (chs.Port1 < chs.Port2);
+                    StatusHandler.CheckStatus();
+                } while (StatusHandler.Port1 < StatusHandler.Port2);
+
                 //** End Fourth Loop **//
+
                 Console.ForegroundColor = ConsoleColor.White; // Change Console Font Color to White
+
                 //** Set all Values of Variables to Default **//
-                chs.Checkinput1 = true;
-                chs.Checkinput2 = true;
-                chs.Checkinput3 = true;
-                chs.Ipeingabe = "";
-                chs.Port1 = 0;
-                chs.Port2 = 0;
+                StatusHandler.Checkinput1 = true;
+                StatusHandler.Checkinput2 = true;
+                StatusHandler.Checkinput3 = true;
+                StatusHandler.Ipeingabe = "";
+                StatusHandler.Port1 = 0;
+                StatusHandler.Port2 = 0;
             } while (true);
             //** End First Loop **//
         }
--- sempt3x Port Scanner/StatusHandler.cs
+++ sempt3x Port Scanner/StatusHandler.cs
@@ -3,21 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace sempt3x_Port_Scanner
 {
-    class StatusHandler
+  static class StatusHandler
     {
-        public string Ipeingabe { get; set; } = "";
+        public static string Ipeingabe { get; set; } = "";
 
-        public int Port1 { get; set; }
+        public static int Port1 { get; set; }
 
-        public int Port2 { get; set; }
+        public static int Port2 { get; set; }
 
-        public bool Checkinput1 { get; set; } = true;
+        public static bool Checkinput1 { get; set; } = true;
 
-        public bool Checkinput2 { get; set; } = true;
+        public static bool Checkinput2 { get; set; } = true;
 
-        public bool Checkinput3 { get; set; } = true;
+        public static bool Checkinput3 { get; set; } = true;
+
+        public static void InputValues()
+        {
+            Console.Write(" Enter the first port: "); // Port 1 Input
+            try
+            {
+                Port1 = Convert.ToInt32(Console.ReadLine());
+                Checkinput2 = string.IsNullOrEmpty(Port1.ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Failed!");
+            }
+            Console.Write(" Enter the second port: "); // Port 2 Input
+            try
+            {
+                Port2 = Convert.ToInt32(Console.ReadLine());
+                Checkinput3 = string.IsNullOrEmpty(Port2.ToString());
+            }
+            catch
+            {
+                Console.WriteLine(" Failed!");
+            }
+        }
+
+        public static void CheckStatus()
+        {
+            var client = new TcpClient();
+            Port1++;
+
+            var result = client.BeginConnect(Ipeingabe, Port1, null, null);
+            var sucess = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+            if (!sucess)
+            {
+                Console.ForegroundColor = ConsoleColor.Red; // If Port closed -> Red
+                Console.WriteLine(" [TCP-Port: " + Port1 + " ] Port is not open!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // If Port open -> Green
+                Console.WriteLine(" [TCP-Port: " + Port1 + " ] Port is open!");
+            }
+        }
     }
 }
